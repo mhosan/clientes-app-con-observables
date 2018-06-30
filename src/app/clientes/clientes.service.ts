e0bd793ebd90cd98bf36a6bcb56c630ad4a1c255
Marcelo Hosan <mhosan@gmail.com>
2018-06-30 20:18:08 -0300

    Update clientes.service.ts
    Agregados comentarios

--- src/app/clientes/clientes.service.ts
+++ src/app/clientes/clientes.service.ts
@@ -1,15 +1,17 @@
 
 import { Injectable } from '@angular/core';
-import {Cliente, Grupo } from './cliente.model';
-import { Observable } from 'rxjs';
-import { Subject } from 'rxjs';
+import {Cliente, Grupo } from './cliente.model';  //el modelo
+import { Observable } from 'rxjs';                //libreria de Observable
+import { Subject } from 'rxjs';                   //libreria de subject (emisor de eventos para el observable)
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
-  private clientes$ = new Subject<Cliente[]>();  //vamos a observar el array de clientes
+  private clientes$ = new Subject<Cliente[]>();  //vamos a observar el array de clientes instanciando una propiedad terminada con el
+                                                 //signo $ para identificar que es del tipo observable y definimos que vamos a observar
+                                                 //el array Cliente, definido en el modelo.
   private clientes: Cliente[];
   private grupos: Grupo[];
 
@@ -58,7 +60,7 @@ export class ClientesService {
      };
    }
 
-   getCliente$(): Observable<Cliente[]>{
-     return this.clientes$.asObservable();
+   getCliente$(): Observable<Cliente[]>{      //este metodo (que tambien termina con $) devuelve un Observable invocando a la prop.
+     return this.clientes$.asObservable();    //cliente$ definida previamente.
    }
 }
